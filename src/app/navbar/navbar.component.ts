643bb9157fea3b21e93024842d420f58dcea722c
oxnoctisxo <tip.hon@hotmail.fr>
2017-02-07 10:12:14 +0100

    Navbar2
    Correct NavBar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { Component,OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent extends OnInit {
+export class NavbarComponent implements OnInit {
   title = 'app works!';
 
   
