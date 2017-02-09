56d2e7c4816d1bd6aaa6e79ffa9f58f211446cb1
oxnoctisxo <tip.hon@hotmail.fr>
2017-02-09 10:18:55 +0100

    Mock logins

--- src/app/services/geslog-api.service.ts
+++ src/app/services/geslog-api.service.ts
@@ -33,7 +33,8 @@ export class GeslogService implements OnInit {
     public getStudLogins(matricule?: number | string) {
         matricule = (!matricule) ? localStorage.getItem('userMatricule') : matricule;
         //return this._http.get(`${this._urlToApi}/logins/${matricule}`).map(res => res.json().data);
-        return this._http.get('app/mocks/api-logins.json').map(res => res.json().data) ;
+         console.log("tentative");
+        return this._http.get('app/mocks/api-logins.json').map(res => res.json().data);
        
     }
 
